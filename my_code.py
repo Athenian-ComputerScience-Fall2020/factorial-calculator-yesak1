cc1ce4b21a80135101daf7116eeb39578d0bc7a4
yesak1 <23kkazliner@athenian.org>
2020-09-16 07:36:55 +0000

    update- sep 16

--- my_code.py
+++ my_code.py
@@ -1,22 +1,33 @@
-# Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  asked my uncle for some tips as he has some python experience.
-number=input("enter number")
-def factorialcalc(x):   #you may choose the name of the parameter
-if number < 0:
-    print("negative values cannot be factorialized")
-elif number==0 :
-    print("the factorial of 0 is 1")
+# Collaborators: I asked my uncle for some tips as he has some python experience.
+def factorialcalc(num):   #you may choose the name of the parameter
+    if num==1:
+        return num
+    else:
+        return num * factorialcalc(num-1)
+        
+
+num=int(input("enter number"))
+
+if num < 0:
+    print ("negative values cannot be factorialized")
+elif num == 0:
+    print ("factorial of 0 is 1")
 else:
-    for x in range (1, number+1):
-        factorial=factorial*x
-    print(f"the factorial of {number} is {factorial}")
-    return number
+    print ("factorial of", num, "is", factorialcalc(num))
+
+
+
+    
+
+  
   # be sure to return the factorial
 if __name__ == '__main__':
     # Test your code with this first
     # Change the argument to try different values
-    print (factorialcalc)
+   
+    print(factorialcalc(int(num)))
+    
 
     # After you are satisfied with your results, use input() to prompt the user for a value:
     #num = input("Enter value to factorialize: ")
-    #print(factorial_calc(int(num)))
+    
